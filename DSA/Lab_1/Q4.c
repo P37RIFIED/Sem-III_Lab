233ff3a6067bb29a7da951f388d8a6f88f21331d
P37RIFIED <105243735+P37RIFIED@users.noreply.github.com>
2023-05-31 20:27:33 +0545

    asdsa

---
+++ DSA/Lab_1/Q4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct record
+{
+    char name[50];
+    int age;
+};
+int main()
+{
+    struct record *recordPtr;
+    recordPtr = (struct record*)malloc(sizeof(struct record));
+
+    printf("Enter the name of the student: ");
+    scanf("%s", recordPtr -> name);
+
+    printf("Enter the age of the student: ");
+    scanf("%d", recordPtr -> age);
+
+    printf("Student Name: %s", recordPtr -> name);
+    printf("Student's Age: %d", recordPtr -> age);
+
+    free(recordPtr);
+
+    return 0;
+}
