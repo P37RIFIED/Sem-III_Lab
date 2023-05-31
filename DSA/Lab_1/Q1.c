49e0e6afa6f70edd83be2af5cf6eb4f14590b151
P37RIFIED <105243735+P37RIFIED@users.noreply.github.com>
2023-05-31 21:22:50 +0545

    Update Q1.c

--- DSA/Lab_1/Q1.c
+++ DSA/Lab_1/Q1.c
@@ -5,14 +5,9 @@
 int main()
 {
     int *number;
-
     number = (int*)malloc(sizeof(int));
-
     *number = 1200;
-
     printf("The number is %d", *number);
-
     free(number);
-
     return 0;
 }
